567b769ed41ecb55865a19c6562157308ff769b4
Salvador Norberto Vega Luna <94280842+senddalton@users.noreply.github.com>
2025-04-20 07:33:51 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Mover botón de contacto a sección de precios
     const contactBtn = document.createElement('button');
     contactBtn.textContent = 'Contáctanos por WhatsApp';
-    contactBtn.className = 'whatsapp-button';
+    contactBtn.className = 'contacto';
     contactBtn.addEventListener('click', () => {
         window.open('https://wa.me/+5216183274838?text=Hola,%20me%20gustaría%20contactarlos', '_blank');
     });
